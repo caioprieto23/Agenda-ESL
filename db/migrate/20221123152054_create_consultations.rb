4118b6d6a71772cc9fcd63edf32ac244d5ae18aa
Caio Hideki Prieto <115494180+caioprieto23@users.noreply.github.com>
2023-01-13 10:59:45 -0300

    Add files via upload

--- db/migrate/20221123152054_create_consultations.rb
+++ db/migrate/20221123152054_create_consultations.rb
@@ -6,7 +6,8 @@ class CreateConsultations < ActiveRecord::Migration[6.1]
       t.datetime :start_time
       t.datetime :end_time
       t.belongs_to :user
-      
+      t.belongs_to :username
+
       t.timestamps
     end
   end
